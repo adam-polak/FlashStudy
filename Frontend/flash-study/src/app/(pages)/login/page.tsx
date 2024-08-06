9d7f1f5d6d8fdcb628a7d41d6a4abeab5a647694
Adam Polak <adamjpolak@gmail.com>
2024-08-06 13:12:07 -0500

    create nav bar

---
+++ Frontend/flash-study/src/app/(pages)/login/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+    return (
+      <main className="">
+        <h1>Login Page</h1>
+      </main>
+    );
+  }
+  
--- Frontend/flash-study/src/app/layout.tsx
+++ Frontend/flash-study/src/app/layout.tsx
@@ -16,7 +16,17 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className="bg-red-400 flex flex-wrap items-center justify-between mx-auto p-4">
+          <button className="self-center text-3xl font-bold whitespace-nowrap text-white">FlashStudy</button>
+          <div className="flex items-center">
+            <button className="flex items-center w-20 h-12 hover:bg-red-700 bg-red-600 rounded-md">
+              <a href="http://youtube.com" className="font-medium p-4 text-xl text-white">Login</a>
+            </button>
+          </div>
+        </div>
+        {children}
+      </body>
     </html>
   );
 }
