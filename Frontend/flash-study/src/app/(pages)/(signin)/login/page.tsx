44a3daba589b6450e64b1b6adc2b4a9017929d12
Adam Polak <adamjpolak@gmail.com>
2024-08-15 15:40:50 -0500

    change spacing

--- Frontend/flash-study/src/app/(pages)/(signin)/login/page.tsx
+++ Frontend/flash-study/src/app/(pages)/(signin)/login/page.tsx
@@ -4,8 +4,8 @@ import { Input } from "@/components/ui/input";
 export default function Home() {
     return (
       <main className="flex flex-row justify-center items-center p-10">
-        <Card className="md:p-10 p-5 md:w-[60%] lg:w-[50%] w-[70%] h-[18em] md:h-[20em] bg-rose-300 text-white font-semibold">
-          <CardContent className="flex flex-col space-y-2">
+        <Card className="md:p-10 p-5 md:w-[60%] lg:w-[50%] w-[100%] h-[20em] bg-rose-300 text-white font-semibold">
+          <CardContent className="flex flex-col space-y-4">
             <div className="flex flex-col space-y-2">
               <p>Username</p>
               <Input className="text-black font-normal focus-visible:ring-rose-400" type="text" placeholder="username"></Input>
