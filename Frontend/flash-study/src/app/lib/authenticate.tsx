f2506a4d0c11d9722e8f3ce17dde2cf86cb3fa69
Adam Polak <adamjpolak@gmail.com>
2024-08-23 14:47:24 -0500

    useEffect can't be called conditionally

--- Frontend/flash-study/src/app/lib/authenticate.tsx
+++ Frontend/flash-study/src/app/lib/authenticate.tsx
@@ -4,5 +4,7 @@ import { useEffect } from "react";
 export async function useAuthentication(searchParams: ReadonlyURLSearchParams) {
     const id = searchParams.get('i') ?? "";
     const router = useRouter();
-    if(id === '' || isNaN(Number(id))) useEffect(() => {router.push('/')});
+    useEffect(() => {
+        if(id === '' || isNaN(Number(id))) router.push('/');
+    })
 }
