dc7adf49848cdce1c27c7524df4eb61c84d7dd7b
Adam Polak <adamjpolak@gmail.com>
2024-08-29 15:56:45 -0500

    calling api to validate login key

--- Frontend/flash-study/src/app/lib/userValidation.tsx
+++ Frontend/flash-study/src/app/lib/userValidation.tsx
@@ -2,9 +2,20 @@ import { ReadonlyURLSearchParams, useRouter } from "next/navigation";
 import { useEffect } from "react";
 
 export async function useAuthentication(searchParams: ReadonlyURLSearchParams) {
-    const id = searchParams.get('i') ?? "";
+    const loginKey = searchParams.get('i') ?? "";
     const router = useRouter();
     useEffect(() => {
-        if(id === '' || isNaN(Number(id))) router.push('/');
+        if(loginKey === '' || isNaN(Number(loginKey))) router.push('/');
+        else {
+            const response = validateLoginKey(loginKey);
+            if(isNaN(Number(response))) {
+            }
+        }
     })
+}
+
+async function validateLoginKey(loginKey: string) : Promise<string> {
+    const apiUrl = 'https://flashstudy-api.azurewebsites.net/loginkey/' + loginKey;
+    const response = fetch(apiUrl);
+    return '';
 }
